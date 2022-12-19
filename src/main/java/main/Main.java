2474f188dbd83e00f4ad9532c7af58ca9b6fd1d3
gu1show <denissolovyov57@gmail.com>
2022-12-19 20:57:10 +0300

    Обработано исключение, убрана библиотека из корня проекта

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,10 @@ package main;
 import factory.Converter;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import validator.InputValidator;
+
+import javax.xml.bind.JAXBException;
 
 /**
  * Запускающий класс.
@@ -12,7 +15,13 @@ import validator.InputValidator;
 public class Main {
     public static void main(@NonNull String[] args) throws Exception {
         log.info("Программа запущена.");
-        new Converter(new InputValidator(args)).convert();
+        try {
+            new Converter(new InputValidator(args)).convert();
+        } catch (JAXBException exception) {
+            val message = "Невозможно создать экземпляр без аргументов у какого-то класса из пакета model.";
+            System.out.println(message);
+            throw new JAXBException(message);
+        }
         log.info("Программа завершена.");
         System.out.println("Конвертация прошла успешно.");
     }
